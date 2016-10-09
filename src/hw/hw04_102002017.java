61a25cfe72a76885a24b8568567e136f9013713f
102002017 <102002017@live.asia.edu.tw>
2016-10-09 12:09:36 +0800

    作業補交

--- src/hw/hw04_102002017.java
+++ src/hw/hw04_102002017.java
@@ -4,10 +4,25 @@ package hw;
  * 讓使用者輸入一正整數n，用while迴圈計算n!後輸出。
  */
 
+import java.util.Scanner;
 public class hw04_102002017 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.print("請輸入一個正整數: ");
+		int n = scn.nextInt();
+		int ans = 1;
+		
+		int i = n;
+		while(i > 0)
+		{
+			ans*=i;
+			i--;
+		}
+		
+		System.out.println(n + "!" + " = " + ans);
 
 	}
 
--- src/hw/hw05_102002017.java
+++ src/hw/hw05_102002017.java
@@ -4,10 +4,24 @@ package hw;
  * 讓使用者輸入一正整數n，計算1*(1+1) + 2*(2+1) + 3*(3+1) + … + n*(n+1)並輸出結果。
  */
 
+import java.util.Scanner;
 public class hw05_102002017 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.print("請輸入一個正整數: ");
+		int n = scn.nextInt();
+		int ans = 0;
+		
+		for(int i = 1; i <= n; i++)
+		{
+			int m = i*(i+1);
+			ans += m;
+		}
+		
+		System.out.println("結果為 " + ans);
 
 	}
 
